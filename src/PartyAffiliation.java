eea2055a2f56d4da62c5b607ac9b34296cd9db31
JimmyJangler <roarknoah2@gmail.com>
2024-02-18 20:29:06 -0500

    Task 3 Lab 05

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,7 +1,36 @@
+import java.util.Scanner;
+
 public class PartyAffiliation
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //scanner and variable declarations
+        Scanner in = new Scanner(System.in);
+        String partyChoice = "";
+        String polParties = "D - Democrat\tR - Republican\tI - Independent\tO - Other";
+
+        //Main code for user input and output statements
+        System.out.println(polParties + "\nChoose your party Affiliation [DRIO]");
+        partyChoice = in.nextLine().toUpperCase();
+        if (partyChoice.equals("D"))
+        {
+            System.out.println("You get a Democratic Donkey.");
+        }
+        else if (partyChoice.equals("R"))
+        {
+            System.out.println("You get a Republican Elephant.");
+        }
+        else if (partyChoice.equals("I"))
+        {
+            System.out.println("You get an Independent Person.");
+        }
+        else if (partyChoice.equals("O"))
+        {
+            System.out.println("You have chosen other.");
+        }
+        else //test for invalid input
+        {
+            System.out.println("You input an illegal value: " + partyChoice);
+        }
     }
 }
