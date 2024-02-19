ffa8b7eb8e4b234760bf38e1d97115bbfa0a00cb
JimmyJangler <roarknoah2@gmail.com>
2024-02-18 20:44:07 -0500

    Task 4 Lab 05

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class TheaterKiosk
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        //scanner and variable declarations
+        Scanner in = new Scanner(System.in);
+        final int DRINKING_AGE = 21;
+        int age = 0;
+        //output statement prompting user for input
+        System.out.println("Enter your age: ");
+        age = in.nextInt();
+        if (age >= DRINKING_AGE) //testing for age to get wrist band
+        {
+            System.out.println("You get a wristband!");
+        }
     }
 }
